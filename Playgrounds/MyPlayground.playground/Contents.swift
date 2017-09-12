b7e4726f05e1590a63dcd6127739187cffb607df
Gwendal Roué <gr@pierlis.com>
2017-09-12 14:01:54 +0200

    KeyPath experiment

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -7,15 +7,48 @@ var configuration = Configuration()
 configuration.trace = { print($0) }
 let dbQueue = DatabaseQueue(configuration: configuration)
 
-try! dbQueue.inDatabase { db in
-    try db.create(table: "persons") { t in
-        t.column("id", .integer).primaryKey()
-        t.column("name", .text)
+struct Player : RowConvertible, MutablePersistable {
+    var id: Int64?
+    let name: String
+    let score: Int
+    
+    // GRDB
+    
+    enum Columns {
+        static let id = Column("id").with(keyPath: \Player.id)
+        static let name = Column("name").with(keyPath: \Player.name)
+        static let score = Column("score").with(keyPath: \Player.score)
     }
     
-    try db.execute("INSERT INTO persons (name) VALUES (?)", arguments: ["Arthur"])
-    try db.execute("INSERT INTO persons (name) VALUES (?)", arguments: ["Barbara"])
+    static var databaseTableName = "players"
     
-    let names = try String.fetchAll(db, "SELECT name FROM persons")
-    print(names)
+    init(row: Row) {
+        id = row[Columns.id]
+        name = row[Columns.name]
+        score = row[Columns.score]
+    }
+    
+    func encode(to container: inout PersistenceContainer) {
+        encode(Columns.id, to: &container)
+        encode(Columns.name, to: &container)
+        encode(Columns.score, to: &container)
+    }
+    
+    mutating func didInsert(with rowID: Int64, for column: String?) {
+        id = rowID
+    }
+}
+
+
+try! dbQueue.inDatabase { db in
+    try db.create(table: "players") { t in
+        t.column("id", .integer).primaryKey()
+        t.column("name", .text)
+        t.column("score", .integer)
+    }
+    
+    var player = Player(row: ["id": 1, "name": "Arthur", "score": 1000])
+    try player.insert(db)
+    
+    try print(Player.filter(Player.Columns.id == 1).fetchOne(db)!.name)
 }
